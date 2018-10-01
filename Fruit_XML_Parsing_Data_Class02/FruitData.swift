478f82ec2add7168136dada840366de3526b01d0
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-10-01 15:57:34 +0900

    Initial Commit

---
+++ Fruit_XML_Parsing_Data_Class02/FruitData.swift
@@ -0,0 +1,17 @@
+//
+//  FruitData.swift
+//  Fruit_XML_Parsing_Data_Class02
+//
+//  Created by D7702_09 on 2018. 10. 1..
+//  Copyright © 2018년 jhi. All rights reserved.
+//
+
+import Foundation
+class FruitData {
+    var name = ""
+    var color = ""
+    var cost = ""
+    
+}
+
+
